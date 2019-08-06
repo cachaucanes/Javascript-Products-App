e71d61aa26656ffa30e88d90b01a5eb8f520b9d7
cachaucanes23 <cachaucanes23@gmail.com>
2019-08-06 16:50:29 -0500

    Proyecto basico de registro y lista de productos

---
+++ app.js
@@ -0,0 +1,77 @@
+class Product {
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
+}
+
+class UI {
+
+  addProduct(product) {
+    const productList = document.getElementById('product-list')
+    const element = document.createElement('div');
+    element.innerHTML = `
+      <div class="card text-center mb-4">
+        <div class="card-body">
+          <strong>Product</strong>: ${product.name}
+          <strong>Product precio</strong>: ${product.price}
+          <strong>Product year</strong>: ${product.year}
+          <a href="#" name="delete" class="btn btn-danger">Delete</a>
+        </div>
+      </div>
+    `;
+    productList.appendChild(element)
+  }
+
+  resetForm() {
+    document.getElementById('product-form').reset()
+  }
+
+  deleteProduct(element) {
+    if (element.name === 'delete') {
+      element.parentElement.parentElement.parentElement.remove()
+      this.showMessage('Product delete successfully', 'danger')
+    }
+  }
+
+  showMessage(message, color) {
+    const div = document.createElement('div')
+    div.className = `alert alert-${color} mt-2`;
+    div.appendChild(document.createTextNode(message))
+    //Mostrando en el dom
+    const container = document.querySelector('.container')
+    const app = document.querySelector('#App')
+    container.insertBefore(div, app)
+    //Tiempo del mensaje
+    setTimeout(function () {
+      document.querySelector('.alert').remove()
+    }, 3000)
+  }
+}
+
+//DOM events
+document.getElementById('product-form').addEventListener('submit', function (e) {
+  const name = document.getElementById('name').value
+  const price = document.getElementById('price').value
+  const year = document.getElementById('year').value
+
+  const product = new Product(name, price, year)
+
+  const ui = new UI()
+  //Verifica que lo campos no esten vacios
+  if (name === '' || price === '' || year === '') {
+    return ui.showMessage('Complete fields please', 'warning')
+  }
+
+  ui.addProduct(product)
+  ui.resetForm()
+  ui.showMessage('Product Added Successfully', 'success')
+  document.getElementById('name').focus()
+  e.preventDefault() //e de Evento que recibe
+})
+
+document.getElementById('product-list').addEventListener('click', function (e) {
+  const ui = new UI()
+  ui.deleteProduct(e.target)
+})
